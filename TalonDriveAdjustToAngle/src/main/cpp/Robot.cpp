eba865b04e55cb0931af557be0f6ad4cd59b5f64
KyleGBC <kyle.roleson@dexterschools.org>
2020-01-27 17:24:48 -0500

    Split repo into Spark code and Talon code (correctly this time?)

---
+++ TalonDriveAdjustToAngle/src/main/cpp/Robot.cpp
@@ -0,0 +1,196 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+
+void Robot::RobotInit() {
+  // Setup for SmartDashboard.
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+  // Input Device Objects
+  joystick_1 = new frc::Joystick(0);
+
+  // Robot Motor Objects
+  left_motor_1 = new WPI_TalonSRX(1);
+  left_motor_2 = new WPI_TalonSRX(4);
+
+  right_motor_1 = new WPI_TalonSRX(2);
+  right_motor_2 = new WPI_TalonSRX(6);
+
+  // Robot Gyroscope Objects
+  ahrs = new AHRS(SPI::Port::kMXP);
+  ahrs->ZeroYaw();
+
+  // PID Controller Object Setup
+  turn_controller = new frc2::PIDController(kP, kI, kD);
+  turn_controller->EnableContinuousInput(kMinInputRange, kMaxInputRange);
+
+  frc::SmartDashboard::PutNumber("P value", kP);
+  frc::SmartDashboard::PutNumber("I value", kI);
+  frc::SmartDashboard::PutNumber("D value", kD);
+}
+
+void Robot::DreadbotTankDrive(double yAxis, double rotAxis, bool checkForDeadband) {
+  // Account for Joystick Deadband
+  if(checkForDeadband) {
+    yAxis = (fabs(yAxis) < kJoystickDeadband) ? 0 : yAxis;
+    rotAxis = (fabs(rotAxis) < kJoystickDeadband) ? 0 : rotAxis;
+  }
+
+  // Flipping Variables to change behavior for rotation influence.
+  rot_speed = (fabs(rotAxis) > 0.0) ? -rot_speed : rot_speed;
+
+  // Multiply Intended Velocity by a cap speed.
+  // (See main.include.Robot.h)
+  y_speed = yAxis * kSpeed;
+  rot_speed = rotAxis * kSpeed;
+  std::cout<<"yAxis: "<<yAxis<<" rotAxis: "<<rotAxis<<std::endl;
+  std::cout<<"y_speed: "<<y_speed<<" rot_speed: "<<rot_speed<<std::endl;
+  // Calculating Final Speed
+  // by Adding the Rotation Factor
+  left_final_speed = y_speed + -rot_speed;
+  right_final_speed = y_speed + rot_speed;
+
+  if(left_final_speed > 1){
+    left_final_speed = 1;
+  }
+  if(left_final_speed < -1){
+    left_final_speed = -1;
+  }
+  if(right_final_speed > 1){
+    right_final_speed = 1;
+  }
+  if(right_final_speed < -1){
+    right_final_speed = -1;
+  }
+
+  // Set Motor Values
+  std::cout << "left_final_speed: " << left_final_speed << " right_final_speed: " << right_final_speed << std::endl;
+  left_motor_1->Set(ControlMode::PercentOutput, left_final_speed);
+  left_motor_2->Set(ControlMode::PercentOutput, left_final_speed);
+
+  right_motor_1->Set(ControlMode::PercentOutput, -right_final_speed);
+  right_motor_2->Set(ControlMode::PercentOutput, -right_final_speed);
+}
+
+void Robot::RotateToAngle(double targetAngle, double currentAngle){ //angle is -180 to 180
+  
+  // turn_controller->SetSetpoint(targetAngle);
+  // turn_controller->Enable();
+  // current_rotation_rate = rotate_to_angle_rate;
+  double localMinSpeed = minRotationRate;
+
+  if (fabs(error) < slop){
+    BUTTON_TIMEOUT++;
+    // localMinSpeed = 0.25;
+  }
+
+  error = currentAngle - targetAngle;
+  current_rotation_rate = (error * kP);
+  if(current_rotation_rate < 0){
+    current_rotation_rate -= localMinSpeed;
+  }
+  else if (current_rotation_rate > 0){
+    current_rotation_rate += localMinSpeed;
+  }
+  // if(error < 0){
+  //   current_rotation_rate = current_rotation_rate * -1;
+  // }
+  
+  if(fabs(error) < slop && BUTTON_TIMEOUT > timeToAdjust){
+    turnComplete = true;
+    ahrs->ZeroYaw();
+  }
+
+  frc::SmartDashboard::PutNumber("rotation_rate", current_rotation_rate);
+  std::cout << "current_rotation rate: " << current_rotation_rate << std::endl;
+  // Rotate to angle using TankDrive function.
+  DreadbotTankDrive(-joystick_1->GetRawAxis(y_axis), current_rotation_rate, false);
+   
+}
+
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::TeleopInit() {
+  double selectedAngle = 0;
+  ahrs->ZeroYaw();
+}
+
+void Robot::TeleopPeriodic() {
+  frc::SmartDashboard::PutNumber("Current Angle", ahrs->GetAngle());
+  
+  kP = frc::SmartDashboard::GetNumber("P value", 0.00);
+  kI = frc::SmartDashboard::GetNumber("I value", 0.00);
+  kD = frc::SmartDashboard::GetNumber("D value", 0.00);
+  turn_controller->SetP(kP);
+  turn_controller->SetI(kI);
+  turn_controller->SetD(kD);
+  frc::SmartDashboard::PutNumber("P", turn_controller->GetP());
+  frc::SmartDashboard::PutNumber("D", turn_controller->GetD());
+  frc::SmartDashboard::PutNumber("rotate to angle rate", rotate_to_angle_rate);
+  std::cout<<"y joystick Axis: "<<-joystick_1->GetRawAxis(y_axis)<<" joystick x Axis: "<<-joystick_1->GetRawAxis(x_axis)<<std::endl;
+  DreadbotTankDrive(-joystick_1->GetRawAxis(y_axis), -joystick_1->GetRawAxis(x_axis), true);
+
+  // Check buttons to rotate to angle.
+  // Inherently Overrides DreadBotTankDrive function.
+
+  if(joystick_1->GetRawButton(x_button)){
+    std::cout << "x button pressed" << std::endl;
+    BUTTON_TIMEOUT = 0;
+    turnComplete = false;
+    selectedAngle = kCardinalDegrees[0]; 
+  }
+  else if(joystick_1->GetRawButton(b_button)){
+    BUTTON_TIMEOUT = 0;
+    turnComplete = false;
+    selectedAngle = kCardinalDegrees[1];
+  }
+  else if(joystick_1->GetRawButton(a_button)){
+    BUTTON_TIMEOUT = 0;
+    turnComplete = false;
+    selectedAngle = kCardinalDegrees[2];
+  }
+  else if (joystick_1->GetRawButton(y_button)){
+    BUTTON_TIMEOUT = 0;
+    turnComplete = false;
+    selectedAngle = kCardinalDegrees[3];
+  }
+  if (!turnComplete){
+    RotateToAngle(selectedAngle, ahrs->GetAngle()); 
+  }
+  //std::cout << "error:" << error << std::endl;
+  //std::cout << "BUTTON_TIMEOUT: " << BUTTON_TIMEOUT << "Turn Complete: " << turnComplete << "selectedAngle: " << selectedAngle << "currentAngle: " << ahrs->GetAngle() << std::endl;
+}
+
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
---
+++ TalonDriveAdjustToAngle/src/main/include/Robot.h
@@ -0,0 +1,113 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#ifndef DREADBOTTANKDRIVE_INCLUDE_ROBOT_H_
+#define DREADBOTTANKDRIVE_INCLUDE_ROBOT_H_
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <AHRS.h>
+
+#include <ctre/Phoenix.h>
+
+#include <frc/Joystick.h>
+#include <frc/controller/PIDController.h>
+#include <frc/PIDOutput.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/TimedRobot.h>
+
+#include <frc/WPILib.h>
+
+#include "RobotUtilities.h"
+
+#endif // DREADBOTTANKDRIVE_INCLUDE_ROBOT_H_
+
+class Robot : public frc::TimedRobot, public PIDOutput {
+public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+
+  void PIDWrite(double output) {
+    rotate_to_angle_rate = output;
+  }
+
+  void RotateToAngle(double target_angle, double current_angle);
+  void DreadbotTankDrive(double y_axis, double rot_axis, bool checkForDeadband);
+
+private:
+  // SMARTDASHBOARD VARIABLES/CONSTANTS
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  // TANK DRIVE CONSTANTS
+  const double kJoystickDeadband = 0.2;
+  const double kSpeed = 1;
+
+  // PID CONSTANTS
+  double kP = 0.1;
+  double kI = 0.00;
+  double kD = 0.00;
+  double kF = 0.00;
+
+  const double kPIDPeriod = 20.0;
+
+  const double kToleranceDegrees = 2.0;
+
+  const double kMinInputRange = -180.0;
+  const double kMaxInputRange = 180.0;
+
+  // PID VARIABLES
+  double rotate_to_angle_rate = 0.0;
+  double current_rotation_rate = 0.0;
+
+  int timeToAdjust = 10;
+  double minRotationRate = 0.275;
+
+  double error = 0.0;
+
+  bool rotate_to_angle = false;
+
+  // JOYSTICK INPUT OBJECTS
+  frc::Joystick *joystick_1;
+
+  // MOTOR OBJECTS
+  WPI_TalonSRX *left_motor_1;
+  WPI_TalonSRX *left_motor_2;
+
+  WPI_TalonSRX *right_motor_1;
+  WPI_TalonSRX *right_motor_2;
+
+  // ROBOT GYRO OBJECTS/VARIABLES
+  AHRS *ahrs;
+  int BUTTON_TIMEOUT = 0;
+  double slop = 2;
+  bool turnComplete = true;
+  double selectedAngle = 0;
+
+  double const kCardinalDegrees[4] = {-30.0, 30.0, 45, 0.0};
+
+  // PID OBJECTS
+  frc2::PIDController *turn_controller;
+  
+  // TANK DRIVE VARIABLES
+  double y_speed;
+  double rot_speed;
+
+  double left_final_speed;
+  double right_final_speed;
+};
